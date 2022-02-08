3a39ff849f53c1427aa518d4fb204cbe1559d8ce
craigdsouza <craigds022@gmail.com>
2022-02-08 12:23:35 +0530

    modified groundwater.py config file to add rasterize params

--- config/groundwater.py
+++ config/groundwater.py
@@ -3,31 +3,37 @@ import inspect
 
 rootFol = Path.cwd()
 
-def get_params(state,dataset='consolidated'):
+def get_params(states_str,dataset='consolidated'):
     stack = inspect.stack()
     frm = inspect.stack()[1]
-    step = frm[1].split(".")[0].split("_")[1].lower()
+    step = frm[1].split(".")[0].split("_")[2].lower()
+    print(step)
 
     if step=="preprocess":        
         path = rootFol.joinpath("data","groundwater","cgwb_stationwise_historical","CGWB_original.csv")
         metacols = ["SNO","STATE","DISTRICT","SITE_TYPE","WLCODE","LON","LAT"]
     elif step=="elevations":
-        path = rootFol.joinpath("outputs","groundwater","csv",state+"_processed.csv")
+        path = rootFol.joinpath("outputs","groundwater","levels","preprocessed",states_str+"_processed.csv")
         metacols = ["SNO","STATE","DISTRICT","SITE_TYPE","WLCODE","LON","LAT"]
     elif step=="rechargedischarge":
-        path = rootFol.joinpath("outputs","groundwater","csv",state+"_processed_wElev.csv")
+        path = rootFol.joinpath("outputs","groundwater","levels","preprocessed",states_str+"_processed.csv")
         metacols = ["SNO","STATE","DISTRICT","SITE_TYPE","WLCODE","LON","LAT"]
     elif step=="rasterize":
-        path = rootFol.joinpath("outputs","groundwater","csv",state+"_processed_wRD.csv")
+        path = rootFol.joinpath("outputs","groundwater","levels","preprocessed",states_str+"_processed.csv")
         metacols = ["SNO","STATE","DISTRICT","SITE_TYPE","WLCODE","LON","LAT"]
     else:
         raise Exception("dataset must be either 'preprocess' or 'elevations'")
     return (path,metacols)
 
-#     elif dataset=="monthly":
-#         cgwbStnFol = dataFol.joinpath("groundwater","cgwb_stationwise_wloc")
-#         path = cgwbStnFol.joinpath("groundwater_StateWiseStationLevelReport_Karnataka_monthly_052020_102020.xls")
-#         metacols = ["STATE","DISTRICT","STATION","Latitude","Longtitude","Station Type"]
-#     elif dataset=="consolidated_processed":
-#         path = rootFol.joinpath("outputs","final","csv",state+"_processed_w"+param+".csv")
-#         metacols = ["SNO","STATE","DISTRICT","SITE_TYPE","WLCODE","LON","LAT"]
+
+def grid_params(gridalg):
+    if gridalg =="gdal:gridinversedistance":
+        return {
+        'POWER':2,    # FOR gridinversedistance
+#         'RADIUS':0.25,    # FOR gridinversedistancenearestneighbor / gridlinear
+#         'RADIUS_1':0.25,    # FOR gridinversedistance / gridnearestneighbor / gridaverage
+#         'RADIUS_2':0.25,    # FOR gridinversedistance  /  gridnearestneighbor / gridaverage
+        'MAX_POINTS':12,    # FOR gridinversedistance 
+        'MIN_POINTS':1,    # FOR gridinversedistance / gridaverage
+        'NODATA': -9999,
+    }
