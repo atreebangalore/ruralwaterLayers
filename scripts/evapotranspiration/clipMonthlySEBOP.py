48685460fb3e5e5d51ce0d2c141d9b28fca7bd54
balakumaran <balakumaran247@gmail.com>
2022-03-08 15:02:16 +0530

    added docstrings clipMonthlySEBOP.py

--- scripts/evapotranspiration/clipMonthlySEBOP.py
+++ scripts/evapotranspiration/clipMonthlySEBOP.py
@@ -1,3 +1,10 @@
+"""
+The SEBOP ET Global image clipped for the National Boundary extent.
+soi_national_boundary.shp at {Home Dir}/Data/gis is required.
+The output SEBOP ET image of getMonthlySEBOP for 
+specified month and year located at {Home Dir}/Data/et/sebop/
+in zip format will be the input for this script.
+"""
 import os,sys
 from pathlib import Path
 from subprocess import check_output
@@ -16,7 +23,20 @@ soi_boundary = Path.home().joinpath("Data","gis","soi_national_boundary.shp")
 
 
 def main():
-    """clips monthly SEBOP ET images to India boundary and saves to file
+    """SEBOP ET image located at {Home Dir}/Data/et/sebop/
+    for the specified month and year clipped for 
+    soi_national_boundary.shp extent, Year and month should
+    be same as getMonthlySEBOP.py arguments.
+    
+    Args:
+        year (YYYY): year for the monthly SEBOP ET images to be fetched.
+        month (MM): specific month for the SEBOP ET image to be downloaded.
+    
+    Example:
+    python Code/atree/scripts/evapotranspiration/clipMonthlySEBOP.py 2019 06
+    
+    Output:
+    extracted global image and clipped tif image in {Home Dir}/Data/et/sebop/
     """
     
     year = sys.argv[1]
@@ -34,7 +54,7 @@ def main():
         tifname = [name for name in zip.namelist() if name.endswith('.tif')][0]
         tifpath = str(dataFol) + "\\" + tifname
         print("path of tif file: ",tifpath)
-  
+
         # extracting all the files
         print('Extracting all the files now...')
         zip.extractall(path=str(dataFol))
@@ -52,9 +72,9 @@ def main():
         print("SEBOP image clipped")
     
     out_meta.update({"driver": "GTiff",
-                 "height": out_image.shape[1],
-                 "width": out_image.shape[2],
-                 "transform": out_transform})
+                "height": out_image.shape[1],
+                "width": out_image.shape[2],
+                "transform": out_transform})
     
     india_tifpath = Path(tifpath).parent.joinpath(Path(tifpath).stem + "_india" + Path(tifpath).suffix)
     
