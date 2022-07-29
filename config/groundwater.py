31d810d82288ed242342306ec3e22640a52b431c
Balakumaran <balakumaran247@gmail.com>
2022-07-29 16:02:18 +0530

    updated: groundwater path and metacols

--- config/groundwater.py
+++ config/groundwater.py
@@ -1,28 +1,29 @@
 from pathlib import Path
 import inspect
 
-rootFol = Path.cwd()
+rootFol = Path.home()
 
-def get_params(states_str,dataset='consolidated'):
-    stack = inspect.stack()
+def get_params(states_str):
     frm = inspect.stack()[1]
     step = frm[1].split(".")[0].split("_")[2].lower()
     print(step)
 
-    if step=="preprocess":        
-        path = rootFol.joinpath("data","groundwater","cgwb_stationwise_historical","CGWB_original.csv")
-        metacols = ["SNO","STATE","DISTRICT","SITE_TYPE","WLCODE","LON","LAT"]
-    elif step=="elevations":
-        path = rootFol.joinpath("Code","atree","outputs","groundwater","levels","preprocessed",states_str+"_processed.csv")
-        metacols = ["SNO","STATE","DISTRICT","SITE_TYPE","WLCODE","LON","LAT"]
-    elif step=="rechargedischarge":
-        path = rootFol.joinpath("Code","atree","outputs","groundwater","levels","preprocessed",states_str+"_processed.csv")
-        metacols = ["SNO","STATE","DISTRICT","SITE_TYPE","WLCODE","LON","LAT"]
-    elif step=="rasterize":
-        path = rootFol.joinpath("Code","atree","outputs","groundwater","levels","preprocessed",states_str+"_processed.csv")
-        metacols = ["SNO","STATE","DISTRICT","SITE_TYPE","WLCODE","LON","LAT"]
+    if step=="preprocess":
+        # path = rootFol.joinpath("Code","atree","data","groundwater",
+                            # "cgwb_stationwise_historical","CGWB_original.csv")
+        # metacols = ["SNO","STATE","DISTRICT","SITE_TYPE","WLCODE","LON","LAT"]
+        path = rootFol.joinpath("Code","atree","data","groundwater","levels",
+                                "level1","CGWB_levels_level1.csv")
+        metacols = ['STATE', 'DISTRICT', 'STATION', 'LAT', 'LON', 'Station Type']
+    elif step in ["elevations", "rechargedischarge", "rasterize"]:
+        # path = rootFol.joinpath("Code", "atree", "outputs", "groundwater", "levels",
+        #                        "preprocessed", f"{states_str}_processed.csv")
+        # metacols = ["SNO","STATE","DISTRICT","SITE_TYPE","WLCODE","LON","LAT"]
+        path = rootFol.joinpath("Code","atree","data","groundwater","levels",
+                                "level2",f"{states_str}_processed.csv")
+        metacols = ['STATE', 'DISTRICT', 'STATION', 'LAT', 'LON', 'Station Type']
     else:
-        raise Exception("dataset must be either 'preprocess' or 'elevations'")
+        raise ValueError("dataset must be either 'preprocess' or 'elevations'")
     return (path,metacols)
 
 
