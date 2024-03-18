01bb620f406eb882755ae67fe2bbc80d03b73407
Balakumaran <balakumaran247@gmail.com>
2024-03-18 17:33:50 +0530

    updated

--- scripts/WSI/PRADAN/intersecting_blocks.py
+++ scripts/WSI/PRADAN/intersecting_blocks.py
@@ -5,13 +5,18 @@ from pathlib import Path
 ee.Initialize()
 
 unique_field = 'village_na'
-buffer_distance = 10000  # 10 kilometers
+buffer_distance = 5000  # 5 kilometers
+statReducer = 'stdDev' # 'median' or 'stdDev'
 
 feature_collection = ee.FeatureCollection('users/balakumaranrm/PRADAN/villages_CH_WB')
+shrug = ee.FeatureCollection('users/balakumaranrm/PRADAN/SHRUG_CH_WB')
 srtm = ee.Image('USGS/SRTMGL1_003').select('elevation')
 srtm_slope = ee.Terrain.slope(srtm)
 
 dataFol = Path(r'C:\Users\atree\Data\slope')
+dataFol.mkdir(parents=True, exist_ok=True)
+vectorFol = dataFol.joinpath('villages')
+vectorFol.mkdir(parents=True, exist_ok=True)
 
 def median_slope(fc, image):
     return image.reduceRegions(
@@ -21,24 +26,38 @@ def median_slope(fc, image):
         crs="EPSG:4326",
     ).getInfo()
 
+def stdDev_slope(fc, image):
+    return image.reduceRegions(
+        collection=fc,
+        reducer=ee.Reducer.stdDev(),
+        scale=30,
+        crs="EPSG:4326",
+    ).getInfo()
+
 def get_slope(fc, image):
-    stats = median_slope(fc, image)
+    if statReducer == 'stdDev':
+        stats = stdDev_slope(fc, image)
+    elif statReducer == 'median':
+        stats = median_slope(fc, image)
+    else:
+        raise AttributeError('statReducer mentioned not correct')
     out_dict = {}
     for feat in stats['features']:
         props = feat['properties']
+        # print(props)
         name = props[unique_field]
-        value = props['median']
+        value = props[statReducer]
         out_dict[name]=value
     return out_dict
 
 def buffer_and_intersect(feature):
     buffered_feature = feature.geometry().buffer(buffer_distance)
 
-    filtered = feature_collection.filterBounds(buffered_feature).aggregate_array(unique_field)
+    filtered = shrug.filterBounds(buffered_feature).aggregate_array(unique_field)
     
     return feature.set('intersects', filtered)
 
-def intersecting(fc):
+def intersecting(fc, selected_list):
     intersecting_features_collection = fc.map(buffer_and_intersect)
     result = intersecting_features_collection.getInfo()
     out_dict = {}
@@ -49,24 +68,28 @@ def intersecting(fc):
         # print(name)
         intersects = props['intersects']
         # print(intersects)
-        if name in intersects:
-            intersects.remove(name)
+        for vname in selected_list:
+            if vname in intersects:
+                intersects.remove(vname)
         out_dict[name]=intersects
     return out_dict
 
 def main():
     print('started!!!')
-    int_dict = intersecting(feature_collection)
-    print(int_dict)
-    slope_dict = get_slope(feature_collection, srtm_slope)
+    fc_dict = feature_collection.getInfo()
+    selected_list = [feat['properties'][unique_field] for feat in fc_dict['features']]
+    int_dict = intersecting(feature_collection, selected_list)
+    # print(int_dict)
+    selected_buffer = feature_collection.geometry().buffer(buffer_distance+1000)
+    slope_dict = get_slope(shrug.filterBounds(selected_buffer), srtm_slope)
     # print(slope_dict)
     final_dict = {}
     for filename, int_villages in int_dict.items():
-        filepath = dataFol.joinpath('villages', f'{filename}_median_slope.csv')
+        filepath = dataFol.joinpath('villages', f'{filename}_{statReducer}_slope.csv')
         out_dict = {
             village: {
                 filename: slope_dict[filename],
-                'median_slope': slope_dict[village],
+                f'{statReducer}_slope': slope_dict[village],
                 'abs_perc_diff': abs(
                     (
                         (slope_dict[village] - slope_dict[filename])
@@ -88,7 +111,7 @@ def main():
                 }
     # print(final_dict)
     final_df = pd.DataFrame(final_dict).T
-    finalpath = dataFol.joinpath('similar_villages_median_slope.csv')
+    finalpath = dataFol.joinpath(f'similar_villages_{statReducer}_slope.csv')
     # print(final_df)
     final_df.to_csv(finalpath)
     print('completed!!!')
