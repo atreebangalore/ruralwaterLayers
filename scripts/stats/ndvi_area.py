b1b6a7f70c40ee005932dfc1ea632920a7816500
Balakumaran <balakumaran247@gmail.com>
2024-05-07 21:59:20 +0530

    updated

--- scripts/stats/ndvi_area.py
+++ scripts/stats/ndvi_area.py
@@ -9,12 +9,13 @@ from pathlib import Path
 from collections import defaultdict
 from calendar import monthrange
 
-unique_field = 'Structure_'
-data_to_use = 'HLS' # Landsat7, Landsat8, HLS or MODIS
-start_year = 2013
-end_year = 2022
+unique_field = 'name'
+data_to_use = 'Landsat7' # Landsat7, Landsat8, HLS or MODIS
+start_year = 2009
+end_year = 2023
 mask_cropland = True
-NDVI_THRESHOLD = 0.3
+NDVI_THRESHOLD = 0.35
+UPPER_NDVI_THRESHOLD = None # if not None
 DW_CROP_LABEL = 4
 
 L7 = ee.ImageCollection("LANDSAT/LE07/C02/T1_TOA")
@@ -31,14 +32,14 @@ HLS = ee.ImageCollection("NASA/HLS/HLSL30/v002")
 # for feature in gj["features"]:
 #     feature["id"] = f'{feature["id"]:04d}'
 # roi = ee.FeatureCollection(gj)
-roi = ee.FeatureCollection('users/balakumaranrm/karauli_500_buffer')
+roi = ee.FeatureCollection('users/balakumaranrm/karauli/karauli_taal_250_buffer')
 
 dataFol = Path.home().joinpath("Data", "ag", "NDVI_area")
 dataFol.mkdir(parents=True, exist_ok=True)
 # print(f"layer to ee: {lyr_name}")
 
-def feature_stats(image: ee.Image, fc: ee.FeatureCollection, threshold: float, scale_val: int):
-    class_image = image.gte(threshold)
+def feature_stats(image: ee.Image, fc: ee.FeatureCollection, threshold: float, scale_val: int, upper_threshold: float):
+    class_image = image.gte(threshold).lte(upper_threshold) if upper_threshold else image.gte(threshold)
     area_image = class_image.multiply(ee.Image.pixelArea())
     area = area_image.reduceRegions(
         collection=fc,
@@ -54,9 +55,9 @@ def get_crop_mask(roi, crop_start, crop_end):
 
 out_dict = {}
 for yr in range(start_year,end_year+1):
-    dict_key = f'ndvi_gte{NDVI_THRESHOLD}{"_DWcropMask" if mask_cropland else ""}_{data_to_use}_{yr}'
+    dict_key = f'ndvi_gte{NDVI_THRESHOLD}{f"_lte{UPPER_NDVI_THRESHOLD}" if UPPER_NDVI_THRESHOLD else ""}{"_DWcropMask" if mask_cropland else ""}_{data_to_use}_{yr}'
     out_dict[dict_key] = {}
-    crop_mask = get_crop_mask(roi, f'{max(yr, 2015)}-10-01', f'{max(yr, 2015)+1}-02-01')
+    crop_mask = get_crop_mask(roi, '2021-06-01', '2022-03-01')#f'{max(yr, 2015)}-10-01', f'{max(yr, 2015)+1}-02-01')
     for mn in range(9,15):
         if mn>12:
             month=mn-12
@@ -99,8 +100,9 @@ for yr in range(start_year,end_year+1):
                 ndvi = ndvi.updateMask(crop_mask)
         else:
             raise ValueError('data not specified correctly!!!')
-        stats = feature_stats(ndvi, roi, NDVI_THRESHOLD, 30)
+        stats = feature_stats(ndvi, roi, NDVI_THRESHOLD, 30, UPPER_NDVI_THRESHOLD)
         for feature in stats['features']:
+            # print(feature['properties'])
             name = feature['properties'][unique_field]
             ndvi_value = feature['properties']['sum']
             out_dict[dict_key][f'{year}-{month:02d}'][name] = ndvi_value
@@ -111,3 +113,5 @@ for year, df_dict in out_dict.items():
     out_file = dataFol.joinpath(f"{year}.csv")
     df.to_csv(out_file)
     print(out_file)
+
+print('Completed!!!')
