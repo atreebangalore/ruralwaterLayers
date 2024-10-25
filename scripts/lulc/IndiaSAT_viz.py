0bcd716df1767704583dfbbb5b2a788d18bb777a
Balakumaran <balakumaran247@gmail.com>
2024-10-25 19:46:45 +0530

    updated: palette

--- scripts/lulc/IndiaSAT_viz.py
+++ scripts/lulc/IndiaSAT_viz.py
@@ -16,18 +16,19 @@ filtered = lulc.filterDate(START_DATE, END_DATE).select(["b1"])
 image = filtered.mosaic()
 
 viz = [
-    "#b2df8a",
-    "#6382ff",
-    "#d7191c",
-    "#f5ff8b",
-    "#dcaa68",
-    "#33a02c",
-    "#50c361",
-    "#000000",
-    "#dac190",
-    "#a6cee3",
-    "#38c5f9",
-    "#6e0002",
+    "#000000", # black (background)
+    "#ff2d00", # red (Built-up)
+    "#fff000", # yellow (water)
+    "#fff000", # yellow (water)
+    "#fff000", # yellow (water)
+    "#ffffff", # white (no class 5)
+    "#397d49", # green (trees)
+    "#cebf86", # khakhi (barrenlands)
+    "#8b9dc3", # light blue (single)
+    "#8b9dc3", # light blue (single)
+    "#222f5b", # dark blue (double)
+    "#222f5b", # dark blue (triple)
+    "#946b2d", # brown (shrub)
 ]
 
 Map.addLayer(image, {"min": 1, "max": 12, "palette": viz}, f"IndiaSAT-{YEAR}")
